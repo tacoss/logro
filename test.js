a02407e57363040f5902041e354d1d2bdc6e17dc
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-03-19 14:52:45 -0600

    Quick IO tests

--- test.js
+++ test.js
@@ -1,10 +1,20 @@
+const io = require('std-mocks');
+const a = require('assert');
+
+io.use();
+
 const Logro = require('./lib');
 const log = Logro.createLogger(__filename);
 
 Logro.setForbiddenFields(['foo']);
+log.message('ONE', { foo: 'bar' }, 1234);
+log.failure(new Error('FAILURE'), 'TWO', { foo: 'bar' }, 1234);
+log.exception(new Error('EXCEPTION'), 'THREE', { foo: 'bar' }, 1234);
+io.restore();
 
-log.message('ON', { foo: 'bar' }, 1234);
+const { stdout, stderr } = io.flush();
 
-// log.failure(new Error('FAIL'), 'ON', { foo: 'bar' }, 1234);
-
-// log.exception(new Error('FAIL'), 'ON', { foo: 'bar' }, 1234);
+a.ok(stderr[0].includes('Error: EXCEPTION'), 'should print errors to stdout');
+a.ok(stdout[0].includes('"data":{}'), 'should skip forbidden fields');
+a.ok(stdout[1].includes('TWO :: FAILURE'), 'should log failures');
+a.ok(stdout[2].includes('EXCEPTION at Object'), 'should log exceptions');

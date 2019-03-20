fd07082d55ade9bc10bd6f264eb1a5510d422d99
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-03-19 20:14:40 -0600

    Cleanup

--- test.js
+++ test.js
@@ -7,14 +7,14 @@ const Logro = require('./lib');
 const log = Logro.createLogger(__filename);
 
 Logro.setForbiddenFields(['foo']);
-log.info('xxx', 'ONE', { foo: 'bar' }, 1234);
-log.failure('TWO', new Error('FAILURE'), { foo: 'bar' }, 1234);
-log.exception('THREE', new Error('EXCEPTION'), { foo: 'bar' }, 1234);
+log.info('ONE', { foo: 'bar' }, 1234);
+log.failure(new Error('FAILURE'), 'TWO', { foo: 'bar' }, 1234);
+log.exception(new Error('EXCEPTION'), 'THREE', { foo: 'bar' }, 1234);
 io.restore();
 
 const { stdout, stderr } = io.flush();
 
 a.ok(stderr[0].includes('Error: EXCEPTION'), 'should print errors to stdout');
 a.ok(stdout[0].includes('"data":{}'), 'should skip forbidden fields');
-a.ok(stdout[1].includes('FAILURE -> TWO'), 'should log failures');
+a.ok(stdout[1].includes('TWO -> FAILURE'), 'should log failures');
 a.ok(stdout[2].includes('EXCEPTION at Object'), 'should log exceptions');
